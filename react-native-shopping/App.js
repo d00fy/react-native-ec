c5a3f400beb3cffa5c10c05032d7ca29f1dbb828
d00fy <doofy2020@icloud.com>
2019-12-20 15:31:00 +0900

    install react-navigation etc

--- react-native-shopping/App.js
+++ react-native-shopping/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// import { createAppContainer } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
+import {
+    createStackNavigator,
+    createBottomTabNavigator,
+    createDrawerNavigator,
+    createAppContainer,
+} from 'react-navigation';
+
 export default function App() {
     return (
         <View style={styles.container}>
