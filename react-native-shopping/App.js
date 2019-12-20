ea6a16488bceba141625f3f9d3c17e8ff9d4c22a
d00fy <doofy2020@icloud.com>
2019-12-20 17:31:29 +0900

    implement fundamental of stack, tab, drawer

--- react-native-shopping/App.js
+++ react-native-shopping/App.js
@@ -1,20 +1,59 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-// import { createAppContainer } from 'react-navigation';
-// import { createStackNavigator } from 'react-navigation-stack';
-import {
-    createStackNavigator,
-    createBottomTabNavigator,
-    createDrawerNavigator,
-    createAppContainer,
-} from 'react-navigation';
+import Single1 from './src/screens/Single1';
+import Single2 from './src/screens/Single2';
+import Stack1 from './src/screens/Stack1';
+import Stack2 from './src/screens/Stack2';
+import Tab1 from './src/screens/Tab1';
+import Tab2 from './src/screens/Tab2';
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
+//stack
+const Stack = createStackNavigator(
+    {
+        Stack1: { screen: Stack1 },
+        Stack2: { screen: Stack2 },
+    },
+    {
+        initialRouteName: 'Stack1',
+    }
+);
+
+Tab;
+const Tab = createBottomTabNavigator({
+    Tab1: { screen: Tab1 },
+    Tab2: { screen: Tab2 },
+    // Tab1: { screen: Stack },
+    // Tab2: { screen: createStackNavigator({ Tab2: { screen: Tab2 } }) },
+});
+
+//drawer
+const Drawer = createDrawerNavigator(
+    {
+        Stacks: { screen: Stack },
+        Tabs: { screen: Tab },
+        Single1: { screen: Single1 },
+        Single2: { screen: Single2 },
+    },
+    {
+        initialRouteName: 'Tabs',
+    }
+);
 
 export default function App() {
+    // const Layout = createAppContainer(AppNavigator);
+    // const Layout = createAppContainer(Tab);
+    const Layout = createAppContainer(Drawer);
     return (
-        <View style={styles.container}>
-            <Text>App.js to start working on your app!</Text>
-        </View>
+        <Layout />
+        // <View style={styles.container}>
+        //     <Single1 />
+        // </View>
     );
 }
 
